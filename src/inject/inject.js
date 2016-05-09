ce08fb465a394a819796e8977c3ff2e0b4d190b5
Bevan Rudge <bevan@js.geek.nz>
2016-05-09 16:34:55 +1200

    Reformat (tidy) Javascript

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,12 +1,12 @@
 chrome.extension.sendMessage({}, function(response) {
-  var readyStateCheckInterval = setInterval(function() {
-    if (document.readyState === "complete") {
-      // This runs when page is done loading.
+    var readyStateCheckInterval = setInterval(function() {
+        if (document.readyState === "complete") {
+            // This runs when page is done loading.
 
-      // TODO Document whe the interval is cleared.
-      clearInterval(readyStateCheckInterval);
-    }
-  }, 10);
+            // TODO Document why the interval is cleared.
+            clearInterval(readyStateCheckInterval);
+        }
+    }, 10);
 
     var COMMENT_TITLE = 'Toggle comments ';
 
@@ -14,7 +14,7 @@ chrome.extension.sendMessage({}, function(response) {
      * Add buttons for toggling comment visibility to each comment thread container.
      */
     function addButtonsIfNotExist() {
-        [].slice.call(document.querySelectorAll('.comment-thread-container')).forEach(function (container) {
+        [].slice.call(document.querySelectorAll('.comment-thread-container')).forEach(function(container) {
             if ($(container).has('button.ba-hide-comment-button').length == 0) {
                 var resolveButton = container.innerText.toLowerCase().indexOf('resolved.') == -1 ? '<button class="ba-resolve-button">resolve</button>' : '';
                 var $toggle = $('<div class="ba-container"><button class="ba-hide-comment-button"></button>' + resolveButton + '</div>');
@@ -34,9 +34,8 @@ chrome.extension.sendMessage({}, function(response) {
      * Case insensitive.
      */
     function hideResolved() {
-        [].slice.call(document.querySelectorAll('.comment-thread-container')).forEach(function (container) {
-            if (container.innerText.toLowerCase().indexOf('resolved.') != -1
-                && !$(container).hasClass('user-override')) {
+        [].slice.call(document.querySelectorAll('.comment-thread-container')).forEach(function(container) {
+            if (container.innerText.toLowerCase().indexOf('resolved.') != -1 && !$(container).hasClass('user-override')) {
                 $(container).addClass('ba-hidden');
             }
         });
@@ -54,10 +53,11 @@ chrome.extension.sendMessage({}, function(response) {
             $(threadContainer).removeClass('ba-hidden').addClass('user-override');
         }
     });
+
     /**
      * Handles clicks on the resolve buttons which are prepended to each comment thread container.
      */
-    $(document).on('click', 'button.ba-resolve-button', function (e) {
+    $(document).on('click', 'button.ba-resolve-button', function(e) {
         var container = $(e.target).closest('.comment-thread-container');
         var replyButton = $(container).find('.reply-link.execute.click')[0];
         replyButton.click();
@@ -67,8 +67,8 @@ chrome.extension.sendMessage({}, function(response) {
         // $(e.target).remove();
     });
 
-    // We don't yet have a way to be event driven, so need to poll for changes.
-    setInterval(function () {
+    // We don't yet have a way to be event-driven, so need to poll for changes.
+    setInterval(function() {
         addButtonsIfNotExist();
         hideResolved();
     }, 2000);
