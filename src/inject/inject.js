066da31d4e8373aa13c7fab7fbde4b2813b75bcc
Bevan Rudge <bevan@js.geek.nz>
2016-05-09 13:35:51 +1200

    reformat

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,33 +1,32 @@
 chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+  var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+      // This runs when page is done loading.
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+      // TODO Document whe the interval is cleared.
+      clearInterval(readyStateCheckInterval);
+    }
+  }, 10);
 
-	}
-	}, 10);
+  function addButtonsIfNotExist() {
+    Array.prototype.slice.call(document.querySelectorAll('.comment-thread-container')).forEach(function(container) {
+      if ($(container).has('button.ba-hide-comment-button').length == 0) {
+        var $toggle = $('<button class="ba-hide-comment-button">Toggle comment</button>')
+        $(container).prepend($toggle);
+      }
+    });
+  }
 
-	function addButtonsIfNotExist(){
-		[].slice.call(document.querySelectorAll('.comment-thread-container')).forEach(function(container){
-			if($(container).has('button.ba-hide-comment-button').length==0){
-				var $toggle = $('<button class="ba-hide-comment-button">Toggle comment</button>')
-				$(container).prepend($toggle);
-			}
-		});
-	}
-	$(document).on('click','button',function(e){
-		var threadContainer = $(e.target).parent();
-		var visible = !threadContainer.hasClass('ba-hidden');
-		if(visible){
-			$(threadContainer).addClass('ba-hidden');
-		}else{
-			$(threadContainer).removeClass('ba-hidden');
-		}
-	});
-	setInterval(addButtonsIfNotExist,2000);
-	console.log('bitbucket awesomizer loaded')
+  $(document).on('click', 'button', function(e) {
+    var threadContainer = $(e.target).parent();
+    var visible = !threadContainer.hasClass('ba-hidden');
+    if (visible) {
+      $(threadContainer).addClass('ba-hidden');
+    } else {
+      $(threadContainer).removeClass('ba-hidden');
+    }
+  });
+
+  setInterval(addButtonsIfNotExist, 2000);
+  console.log('bitbucket awesomizer loaded')
 });
